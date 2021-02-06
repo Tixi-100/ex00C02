fd168b781ab82281b9177cf08583133b6b1a9d95
phorn-21 <lwrow@rambler.ru>
2021-02-06 22:46:55 +0300

    ft_strcpy.c

---
+++ ft_strcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phorne <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/06 20:34:21 by phorne            #+#    #+#             */
+/*   Updated: 2021/02/06 22:43:23 by phorne           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+
+char		*ft_strcpy(char *dest, char *src);
+
+char ft_strcpy(char *dest, char *src)
+{
+	char l[] = "my char";
+	char *src;
+	src = &l;
+	char dst[];
+	int a = 0;
+	while (a < 10)
+	{
+	
+	}
+}
